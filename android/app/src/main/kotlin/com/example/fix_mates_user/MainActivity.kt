e8827bc9d0c5c1180601197d86e5e70de47de9ac
Dhananjay Hareesh <dhananjay.hareesh@gmail.com>
2024-03-22 11:41:12 +0530

    intial commit

---
+++ android/app/src/main/kotlin/com/example/fix_mates_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fix_mates_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:fix_mates_user/view/homescreen.dart';
+import 'package:fix_mates_user/view/opening_screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fix Mates App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
